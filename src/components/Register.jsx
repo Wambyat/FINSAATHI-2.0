4e54ee05fe76126fcf491e69bf8e13844b53de31
aneela-fm <aneela@forwardmeasure.com>
2024-07-15 12:06:19 +0530

    Login and Register css changes

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import "./Register.css";
 
-/// File is incomplete. You need to add input boxes to take input for users to register.
 function Register() {
+    const [name, setName] = React.useState("");
     const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
-    return <div>
-        <h1>Register to the website</h1>
-        <br/>
-        <input type={"text"} onChange={e => setEmail(e.target.value)} />
-        <br/>
-        Already a user? <a href="/login">Login</a>
-    </div>
+    return (
+        <div className="register-container">
+            <h1>Register to the website</h1>
+            <br/>
+            <label>Name</label>
+            <input type="text" onChange={e => setName(e.target.value)} />
+            <br/>
+            <label>Email</label>
+            <input type="text" onChange={e => setEmail(e.target.value)} />
+            <br/>
+            <label>Password</label>
+            <input type="password" onChange={e => setPassword(e.target.value)} />
+            <br/>
+            <button>Register</button>
+            <br/>
+            Already a user? <a href="/Login">Login</a>
+        </div>
+    );
 }
 
 export default Register;
