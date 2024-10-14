dadfd1bb79361d817fa0a830e270a677e4e79883
Anirudha Anekal <anirudha.anekal.work@gmail.com>
2024-10-14 11:10:50 +0530

    More verbose runner script.

--- runner.sh
+++ runner.sh
@@ -1,5 +1,9 @@
+echo "Starting runner script."
 cd src
+echo "Installing node packages."
 npm install
+echo "Installed node packages. Starting the front end."
 npm run dev &
+echo "Started front end. Starting back end."
 cd backend
 node index.js
